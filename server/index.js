08b17b314965209a25dd829851196b995d27da85
Danny Maziad <dmaziad@gmail.com>
2020-01-29 16:48:38 -0500

    Bare bones express server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+const data = require('../data/eventList');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, () => {
+  console.log('Listening on port ', port);
+});
