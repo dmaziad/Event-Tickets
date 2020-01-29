681c9f72fd2374f788b1ef67f9f9696389b9a2a6
Danny Maziad <dmaziad@gmail.com>
2020-01-29 18:45:24 -0500

    Reformatted Time in EventItem

--- client/src/components/EventItem.jsx
+++ client/src/components/EventItem.jsx
@@ -1,16 +1,41 @@
 import React from 'react';
 
 const EventItem = ({ event }) => {
+  // I do have some reservations about assigning "Date" variable
+  // that may conflict with Date keyword, however I went this route
+  // for the sake of uniformity
+  const { Day, Date, Time, EventName, VenueName, MinPrice } = event;
+
+  const adjustTime = time => {
+    const hour = time.slice(0, 2);
+    if (Number(hour) === 0) {
+      return `12${time.slice(2, 5)} AM`;
+    }
+    if (Number(hour) === 12) {
+      return `12${time.slice(2, 5)} PM`;
+    }
+    if (Number(hour) > 12) {
+      const adjustedHour = Number(hour) - 12;
+      return `${adjustedHour + time.slice(2, 5)} PM`;
+    }
+    if (Number(hour) > 12) {
+      const adjustedHour = Number(hour) - 12;
+      return `${time} AM`;
+    }
+  };
+
+  const adjustedTime = adjustTime(Time);
+
   return (
     <tr className="event-item">
       <td className="event-date">
-        {event.Day} {event.Date}
+        {Day} {Date}
       </td>
-      <td className="event-time">{event.Time}</td>
+      <td className="event-time">{adjustedTime}</td>
       <td className="event-name">
-        {event.EventName} - {event.VenueName}
+        {EventName} - {VenueName}
       </td>
-      <td className="event-price">{event.MinPrice}</td>
+      <td className="event-price">{MinPrice}</td>
     </tr>
   );
 };
